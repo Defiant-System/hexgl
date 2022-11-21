7c62e79bb63c5e9716965ec6de824628653ea6fc
Hakan Bilgin <hbi99@hotmail.com>
2022-11-21 22:40:31 +0100

    added resource files

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,79 @@
 
+@import "libs/Three.dev.js"
+@import "libs/ShaderExtras.js"
+@import "libs/postprocessing/EffectComposer.js"
+@import "libs/postprocessing/RenderPass.js"
+@import "libs/postprocessing/BloomPass.js"
+@import "libs/postprocessing/ShaderPass.js"
+@import "libs/postprocessing/MaskPass.js"
+
+
+@import "bkcore/threejs/RenderManager.js"
+@import "bkcore/threejs/Shaders.js"
+@import "bkcore/threejs/Particles.js"
+@import "bkcore/threejs/Loader.js"
+
+@import "bkcore/Timer.js"
+@import "bkcore/ImageData.js"
+@import "bkcore/Utils.js"
+
+
+@import "bkcore/hexgl/HUD.js"
+@import "bkcore/hexgl/RaceData.js"
+@import "bkcore/hexgl/ShipControls.js"
+@import "bkcore/hexgl/ShipEffects.js"
+@import "bkcore/hexgl/CameraChase.js"
+@import "bkcore/hexgl/Gameplay.js"
+
+@import "bkcore/hexgl/tracks/Cityscape.js"
+@import "bkcore/hexgl/HexGL.js"
+
+
+/*
+
+bkcore/Audio.js
+launch.js
+
+*/
+
+console.log( THREE );
+
+
+let game;
+
 const hexgl = {
 	init() {
 		// fast references
-		this.content = window.find("content");
+		this.els = {
+			content: window.find("content"),
+			gameOver: window.find(".game-over"),
+			cvsOverlay: window.find("canvas.overlay"),
+			cvsMain: window.find("canvas.main"),
+		};
+
+		// this.dispatch({ type: "new-game" });
 	},
 	dispatch(event) {
+		let Self = hexgl,
+			el;
 		switch (event.type) {
 			case "window.init":
+				break;
+			case "new-game":
+				game = new bkcore.hexgl.HexGL({
+					// document: document,
+					width: window.innerWidth,
+					height: window.innerHeight,
+					container: Self.els.cvsMain,
+					overlay: Self.els.cvsOverlay,
+					gameover: Self.els.gameOver,
+					track: "Cityscape",
+					controlType: 0,
+					difficulty: 0,
+					quality: 3,
+					godmode: 1,
+					hud: 1,
+				});
 				break;
 			case "open-help":
 				karaqu.shell("fs -u '~/help/index.md'");
@@ -16,3 +83,4 @@ const hexgl = {
 };
 
 window.exports = hexgl;
+ 
